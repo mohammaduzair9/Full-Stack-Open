2152318ed0b594ea0a8bc58bb59e7de75671e6f7
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-20 02:50:19 +0200

    exercise 1.7 unicafe step 2

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -8,18 +8,60 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    const updatedTotal = updatedGood + bad + neutral
+    const updatedAverage = (updatedGood - bad)/ updatedTotal
+    const updatedPositive = (updatedGood / updatedTotal) * 100
+
+    setGood(updatedGood)
+    setTotal(updatedTotal) 
+    setAverage(updatedAverage) 
+    setPositive(updatedPositive)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    const updatedTotal = good + updatedBad + neutral
+    const updatedAverage = (good - updatedBad)/ updatedTotal
+    const updatedPositive = (good / updatedTotal) * 100
+
+    setBad(updatedBad)
+    setTotal(updatedTotal) 
+    setAverage(updatedAverage) 
+    setPositive(updatedPositive)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    const updatedTotal = good + bad + updatedNeutral
+    const updatedAverage = (good - bad)/ updatedTotal
+    const updatedPositive = (good / updatedTotal) * 100
+
+    setNeutral(updatedNeutral)
+    setTotal(updatedTotal) 
+    setAverage(updatedAverage) 
+    setPositive(updatedPositive)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
 
       <h1>statistics</h1>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value={total} />
+      <Display text="average" value={average} />
+      <Display text="positive" value={positive + " %"} />
     </div>
   )
 }
