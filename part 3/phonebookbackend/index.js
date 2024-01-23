15c68facb051c10b0d898e2daf891be5cc490ff9
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-23 16:09:47 +0200

    exercise 3.16 phonebook database step 4

--- part 3/phonebookbackend/index.js
+++ part 3/phonebookbackend/index.js
@@ -32,21 +32,25 @@ app.get('/api/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
     response.json(person)
   })
+  .catch(error => next(error))
 })
 
 // POST Methods //
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const body = request.body
 
   if(!body.name || !body.number){
@@ -65,20 +69,45 @@ app.post('/api/persons/', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(person)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
-
 
 // DELETE Methods //
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
+// UNKNOWN ENDPOINTS //
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// ERROR HANDLER MODULE //
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
