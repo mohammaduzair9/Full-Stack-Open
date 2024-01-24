ae6344f6f8432e45e15ec2b5b1ac208e76f4070c
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-24 13:54:53 +0200

    exercise 4.8 blog list tests step 1

---
+++ part 4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    expect(titles).toContain(
+      'Type wars'
+    )
+  })
+})
+afterAll(async () => {
+  await mongoose.connection.close()
+})
