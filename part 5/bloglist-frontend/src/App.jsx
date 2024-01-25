73981b6f6b011fb46848c6c21a8dc95431695c79
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-25 23:49:44 +0200

    exercise 5.1 bloglist frontend step 1

---
+++ part 5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  const getBlogs = async () => {
+    const blogs = await blogService.getAll()
+    setBlogs(blogs)  
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      console.log(user)
+      setUsername('')
+      setPassword('')
+      getBlogs()
+    } catch (exception) {
+      console.log("Wrong credentials")
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>    
+    </div>  
+  )
+
+  const blogLists = () => ( 
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && loginForm()} 
+      {user && blogLists()}
+        
+    </div>
+  )
+}
+
+export default App
