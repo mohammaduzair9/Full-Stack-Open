496718e976ed1cbabc9b9680c35274fc296255db
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-27 01:01:52 +0200

    exercise 5.15 blog list tests step 3

--- part 5/bloglist-frontend/src/components/Blog.test.js
+++ part 5/bloglist-frontend/src/components/Blog.test.js
@@ -22,13 +22,12 @@ describe('<Blog />', () => {
     token: 'mytesttoken'
   }
 
-  beforeEach(() => {
+
+  test('blog title and author rendered by default, but URL or likes not rendered by default', () => {
+
     container = render(
       <Blog user={ testUser } blog={ testBlog } addLike={ () => null } deleteBlog={ () => null } />
     ).container
-  })
-
-  test('blog title and author rendered by default, but URL or likes not rendered by default', () => {
 
     const div1 = container.querySelector('.defaultblogDetail')
     expect(div1).not.toHaveStyle('display: none')
@@ -40,12 +39,33 @@ describe('<Blog />', () => {
 
   test('URL and likes are shown when button controlling the shown details has been clicked', async () => {
 
+    container = render(
+      <Blog user={ testUser } blog={ testBlog } addLike={ () => null } deleteBlog={ () => null } />
+    ).container
+
     const user = userEvent.setup()
     const button = screen.getByText('view')
     await user.click(button)
 
     const div = container.querySelector('.hiddenblogDetail')
     expect(div).not.toHaveStyle('display: none')
+
+  })
+
+  test('if the like button is clicked twice, the event handler the component received as props is called twice', async () => {
+
+    const mockHandler = jest.fn()
+
+    container = render(
+      <Blog user={ testUser } blog={ testBlog } addLike={ mockHandler } deleteBlog={ () => null } />
+    ).container
+
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
 
   })
 
