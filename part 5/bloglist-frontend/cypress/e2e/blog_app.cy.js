076a6782027ed4344f3389bd19fdfc9c119844f5
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-27 13:41:57 +0200

    exercise 5.17 bloglist end to end testing step 1

---
+++ part 5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,15 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.get('#username').type('user')
+    cy.get('#password').type('pass')
+    cy.get('#login-button').click()
+  })
+})
--- part 5/bloglist-frontend/src/App.jsx
+++ part 5/bloglist-frontend/src/App.jsx
@@ -129,6 +129,7 @@ const App = () => {
       <form onSubmit={handleLogin}>
         <div>
           username<input
+            id='username'
             type="text"
             value={username}
             name="Username"
@@ -137,13 +138,14 @@ const App = () => {
         </div>
         <div>
           password<input
+            id='password'
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   )
