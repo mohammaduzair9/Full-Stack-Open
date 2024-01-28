8fb47358235496fc591c99bb9223cca5d2b9bcca
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-28 11:43:33 +0200

    exercise 6.14 anecdotes and the backend step 1

--- part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,7 @@ const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState: initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload
@@ -43,9 +43,12 @@ const anecdoteSlice = createSlice({
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote 
       )
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, addVote } = anecdoteSlice.actions
+export const { createAnecdote, addVote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
