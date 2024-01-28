b68452980f31cc3f880304e011dd01130f5061c1
Mohammad Uzair <mohammad.uzair@op.fi>
2024-01-28 21:36:20 +0200

    exercise 6.22 anecdotes react query step 3

--- part 6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part 6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,5 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes, createAnecdote } from '../requests'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
 
 const AnecdoteForm = () => {
 
